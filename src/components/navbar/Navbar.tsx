c31fbb727609ee0584c2df9b2ac910694c1a2f18
Martin Fuentes <mefuentes@uninorte.edu.co>
2022-06-23 00:09:57 -0500

    Add navbar and home design

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./Navbar.scss";
 
 export function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <div className="navbar__title">Euphorichuman</div>
+      <div className="navbar__options">
+        <div className="navbar__option">About</div>
+        <div className="navbar__option">Projects</div>
+        <div className="navbar__option">Contact</div>
+      </div>
+      <div className="navbar__actions">
+        <button className="outline-btn">GET IN TOUCH</button>
+      </div>
+    </nav>
+  );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import { Navbar } from "components/navbar/Navbar";
+import "./Home.scss";
+
+import { ReactComponent as StarSvg } from "../../../src/assets/shapes/star.svg";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <Fragment>
+      <Navbar />
+      <section className="home">
+        <div className="home__large-text">
+          <span className="row">
+            <span className="fake-btn normal-text">
+              Hi, my name is Martin Fuentes, I'm
+            </span>
+            <span className="text-primary">FULL-STACK</span>
+          </span>
+          WEB DEVELOPER, AND
+          <br />
+          <span className="text-primary">
+            <span className="text-outline">DDDDD</span>ESIGNER
+          </span>
+        </div>
+
+        <div className="home__shape home__shape-1">
+          <StarSvg />
+        </div>
+        <div className="home__shape home__shape-2">
+          <StarSvg />
+        </div>
+      </section>
+    </Fragment>
+  );
 }
