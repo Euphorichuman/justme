ed468097039159b324bf62d3f5e5c7db532baafa
Euphorichuman <fuentes.acosta.martin@gmail.com>
2025-10-16 16:48:38 -0500

    Fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import { Fragment, useRef } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Home } from "pages/home/Home";
